ead2aa480fa3ac9381bbeb20b8e2944b878428c4
Giampaolo Tranchida <giampaolo.tranchida@vd.ch>
2025-02-13 18:00:21 +0100

    add detailed host information including CPU usage, load average, swap memory, network interfaces, running processes, kernel version, and boot time

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"html/template"
 	"io"
+	"net"
 	"net/http"
 	"os"
 	"time"
@@ -15,26 +16,35 @@ import (
 	"github.com/shirou/gopsutil/v4/cpu"
 	"github.com/shirou/gopsutil/v4/disk"
 	"github.com/shirou/gopsutil/v4/host"
+	"github.com/shirou/gopsutil/v4/load"
 	"github.com/shirou/gopsutil/v4/mem"
+	"github.com/shirou/gopsutil/v4/process"
 	"github.com/shirou/gopsutil/v4/sensors"
 )
 
 type HostInfo struct {
-	CurrentTime     string
-	Hostname        string
-	Port            string
-	Uptime          string
-	OS              string
-	Platform        string
-	PlatformVersion string
-	CPUP            int
-	CPUV            int
-	TotalMemory     string
-	CacheMemory     string
-	FreeMemory      string
-	TotalDiskSpace  string
-	FreeDiskSpace   string
-	CPUTemperature  string
+	CurrentTime       string
+	Hostname          string
+	Uptime            string
+	OS                string
+	Platform          string
+	PlatformVersion   string
+	CPUP              int
+	CPUV              int
+	TotalMemory       string
+	CacheMemory       string
+	FreeMemory        string
+	TotalDiskSpace    string
+	FreeDiskSpace     string
+	CPUTemperature    string
+	CPUUsage          string
+	LoadAverage       string
+	TotalSwap         string
+	FreeSwap          string
+	NetworkInterfaces []string
+	RunningProcesses  int
+	KernelVersion     string
+	BootTime          string
 }
 
 //go:embed static templates
@@ -86,7 +96,6 @@ func newEcho() (*echo.Echo, error) {
 
 	e.GET("/host", func(c echo.Context) error {
 		hostname, _ := os.Hostname()
-		port := "8080"
 
 		// extract information from the current host using gopsutil
 		uptime, _ := host.Uptime()
@@ -109,22 +118,56 @@ func newEcho() (*echo.Echo, error) {
 			}
 		}
 
+		// Get CPU usage
+		cpuPercent, _ := cpu.Percent(0, false)
+		cpuUsage := fmt.Sprintf("%.2f%%", cpuPercent[0])
+
+		// Get load average
+		loadAvg, _ := load.Avg()
+		loadAverage := fmt.Sprintf("%.2f, %.2f, %.2f", loadAvg.Load1, loadAvg.Load5, loadAvg.Load15)
+
+		// Get swap memory
+		swap, _ := mem.SwapMemory()
+
+		// Get network interfaces
+		netInterfaces, _ := net.Interfaces()
+		var interfaces []string
+		for _, iface := range netInterfaces {
+			interfaces = append(interfaces, iface.Name)
+		}
+
+		// Get number of running processes
+		processes, _ := process.Pids()
+
+		// Get kernel version
+		kernelVersion := info.KernelVersion
+
+		// Get boot time
+		bootTime := time.Unix(int64(info.BootTime), 0).Format(time.RFC3339)
+
 		hostInfo := HostInfo{
-			CurrentTime:     time.Now().Format(time.RFC3339),
-			Hostname:        hostname,
-			Port:            port,
-			Uptime:          formatDuration(time.Duration(uptime) * time.Second),
-			OS:              info.OS,
-			Platform:        info.Platform,
-			PlatformVersion: info.PlatformVersion,
-			CPUP:            cpuCountP,
-			CPUV:            cpuCountV,
-			TotalMemory:     humanize.IBytes(memory.Total),
-			FreeMemory:      humanize.IBytes(memory.Free),
-			CacheMemory:     humanize.IBytes(memory.Cached),
-			TotalDiskSpace:  humanize.IBytes(disk.Total),
-			FreeDiskSpace:   humanize.IBytes(disk.Free),
-			CPUTemperature:  cpuTemp,
+			CurrentTime:       time.Now().Format(time.RFC3339),
+			Hostname:          hostname,
+			Uptime:            formatDuration(time.Duration(uptime) * time.Second),
+			OS:                info.OS,
+			Platform:          info.Platform,
+			PlatformVersion:   info.PlatformVersion,
+			CPUP:              cpuCountP,
+			CPUV:              cpuCountV,
+			TotalMemory:       humanize.IBytes(memory.Total),
+			FreeMemory:        humanize.IBytes(memory.Free),
+			CacheMemory:       humanize.IBytes(memory.Cached),
+			TotalDiskSpace:    humanize.IBytes(disk.Total),
+			FreeDiskSpace:     humanize.IBytes(disk.Free),
+			CPUTemperature:    cpuTemp,
+			CPUUsage:          cpuUsage,
+			LoadAverage:       loadAverage,
+			TotalSwap:         humanize.IBytes(swap.Total),
+			FreeSwap:          humanize.IBytes(swap.Free),
+			NetworkInterfaces: interfaces,
+			RunningProcesses:  len(processes),
+			KernelVersion:     kernelVersion,
+			BootTime:          bootTime,
 		}
 
 		return c.Render(http.StatusOK, "main", hostInfo)
