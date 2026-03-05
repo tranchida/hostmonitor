9da4fa2f17654c9a9d037f2a08e63069f9fa48a7
Giampaolo Tranchida <giampaolo@tranchida.ch>
2026-03-06 00:34:05 +0100

    remove svelte files

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BfaCA5dj.js","_app/immutable/chunks/BBT1V1ic.js","_app/immutable/chunks/CtuWOtrZ.js","_app/immutable/chunks/B0vb9z3P.js","_app/immutable/chunks/DDxd7z1_.js","_app/immutable/chunks/DnsSx_Tq.js","_app/immutable/chunks/B9a050j0.js"];
-export const stylesheets = [];
-export const fonts = [];
--- build/server/chunks/1-B2ZsChhU.js
+++
@@ -1,9 +0,0 @@
-const index = 1;
-let component_cache;
-const component = async () => component_cache ??= (await import('./error.svelte-B5C6OG__.js')).default;
-const imports = ["_app/immutable/nodes/1.BfaCA5dj.js","_app/immutable/chunks/BBT1V1ic.js","_app/immutable/chunks/CtuWOtrZ.js","_app/immutable/chunks/B0vb9z3P.js","_app/immutable/chunks/DDxd7z1_.js","_app/immutable/chunks/DnsSx_Tq.js","_app/immutable/chunks/B9a050j0.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-B2ZsChhU.js.map
